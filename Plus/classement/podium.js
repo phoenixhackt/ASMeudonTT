b39debca1120b70147dbb9c71264177534801c6a
phoenixhackt <125221177+phoenixhackt@users.noreply.github.com>
2023-03-17 19:30:43 +0100

    cursor

---
+++ Plus/classement/podium.js
@@ -0,0 +1,6 @@
+// Inclure la bibliothèque tilt.js
+
+// Appliquer l'effet de bascule tilt.js à chaque étape du podium
+$(".first-place").tilt();
+$(".second-place").tilt();
+$(".third-place").tilt();
