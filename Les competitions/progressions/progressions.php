a7e436c709896f871d2fb224e25bdb2331b51fd0
Gary <glaxlax11@gmail.com>
2024-01-02 21:00:42 +0100

    update loader

--- Les competitions/progressions/progressions.php
+++ Les competitions/progressions/progressions.php
@@ -21,39 +21,6 @@
 
     <!-- htmx -->
     <script src="https://unpkg.com/htmx.org@1.9.10" integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC" crossorigin="anonymous"></script>
-
-    <style>
-        #loader-wrapper {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(255, 255, 255, 0.8);
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            z-index: 9999;
-        }
-
-        #loader {
-            border: 8px solid #f3f3f3;
-            border-top: 8px solid #3498db;
-            border-radius: 50%;
-            width: 50px;
-            height: 50px;
-            animation: spin 1s linear infinite;
-        }
-
-        @keyframes spin {
-            0% { transform: rotate(0deg); }
-            100% { transform: rotate(360deg); }
-        }
-
-        .tables {
-            display: none;
-        }
-    </style>
 </head>
 
 <body>
@@ -154,7 +121,9 @@
         <div id="loader-wrapper">
             <div id="loader"></div>
         </div>
-    
+        <div class="precontent">
+        </div>
+                
         <div class="tables">
             <div hx-get="loaddata.php" hx-trigger="load delay:0.1s" hx-swap="outerHTML">
             </div>
@@ -164,9 +133,11 @@
             document.addEventListener("htmx:afterRequest", function () {
                 // Hide the loader and show the content after each htmx request
                 const loaderWrapper = document.getElementById("loader-wrapper");
+                const preContent = document.querySelector(".precontent");
                 const tables = document.querySelector(".tables");
 
                 loaderWrapper.style.display = "none";
+                preContent.style.display = "none";
                 tables.style.display = "block";
             });
         </script>
