30267bd3d91141b40ad9072e2667e9d87edf6495
Gary <glaxlax11@gmail.com>
2024-01-03 21:54:09 +0100

    update

--- Les competitions/championnat_par_equipes/equipes.php
+++ Les competitions/championnat_par_equipes/equipes.php
@@ -41,7 +41,10 @@
                             <a href="../criterium/criterium.html">Criterium</a>
                         </li>
                         <li>
-                            <a href="#">Progressions</a>
+                            <a href="../progressions/progressions.php">Progressions</a>
+                        </li>
+                        <li>
+                            <a href="../championnat_par_equipes/equipes.php">Championnat par equipes</a>
                         </li>
                     </ul>
                 </li>
@@ -81,10 +84,13 @@
                     <li><a class="aPrincipaux" href="#">Les Compétitions</a></li>
                     <ul class="sousNav">
                         <li>
-                            <a class="aSidenav" href="Les competitions/criterium/criterium.html">Criterium</a>
+                            <a class="aSidenav" href="../criterium/criterium.html">Criterium</a>
                         </li>
                         <li>
-                            <a class="aSidenav" href="Les competitions/progressions/progressions.php">Progressions</a>
+                            <a class="aSidenav" href="../progressions/progressions.php">Progressions</a>
+                        </li>
+                        <li>
+                            <a class="aSidenav" href="../championnat_par_equipes/equipes.php">Championnat par equipes</a>
                         </li>
                     </ul>
                     <li><a class="aPrincipaux" href="#">Plus</a></li>
@@ -114,166 +120,171 @@
 
     <main>
 
-        <?php
-        require_once('Fftt/Service.php');
+<?php
+require_once('Fftt/Service.php');
 
-        // Remplacez ces valeurs par celles de votre application FFTT
-        $appId = 'SW927';
-        $appKey = 'uJ5d9P6NqG';
+// Remplacez ces valeurs par celles de votre application FFTT
+$appId = 'SW927';
+$appKey = 'uJ5d9P6NqG';
 
-        // Instancier la classe Service
-        $service = new Mping\CoreBundle\Fftt\Service($appId, $appKey);
+// Instancier la classe Service
+$service = new Mping\CoreBundle\Fftt\Service($appId, $appKey);
 
-        // Initialiser l'utilisateur (à faire une fois)
-        $service->initialization();
+// Initialiser l'utilisateur (à faire une fois)
+$service->initialization();
 
-        // Remplacez NUMERO_CLUB par le numéro de votre club
-        $clubDetails = $service->getClub('08920067');
-        $getJoueursByClub = $service->getJoueursByClub('08920067');
-        $equipesByClub = $service->getEquipesByClub('08920067');
-        // $getEpreuves = $service->getEpreuves('16', 'E');
-        // $getDivisions = $service->getDivisions('F', '257', 'E');
-        // Pour récupérer les id equipes, il suffit d'aller sur le lien de redirection xml_equipe.php et de regarder l'iddiv et cx_poule dans liendivision
-        $getPouleClassementR1 = $service->getPouleClassement('126126', '656628');
-        $getPouleClassementR2 = $service->getPouleClassement('125922', '655864');
-        $getPouleClassementD1_P2 = $service->getPouleClassement('125705', '655054');
-        $getPouleClassementD1_P1 = $service->getPouleClassement('125705', '655053');
-        $getPouleClassementD2_P6 = $service->getPouleClassement('125705', '655058');
-        ?>
+// Remplacez NUMERO_CLUB par le numéro de votre club
+$clubDetails = $service->getClub('08920067');
+$getJoueursByClub = $service->getJoueursByClub('08920067');
+$equipesByClub = $service->getEquipesByClub('08920067');
+// $getEpreuves = $service->getEpreuves('16', 'E');
+// $getDivisions = $service->getDivisions('F', '257', 'E');
+// Pour récupérer les id equipes, il suffit d'aller sur le lien de redirection xml_equipe.php et de regarder l'iddiv et cx_poule dans liendivision
+$getPouleClassementR1 = $service->getPouleClassement('126126', '656628');
+$getPouleClassementR2 = $service->getPouleClassement('125922', '655864');
+$getPouleClassementD1_P2 = $service->getPouleClassement('125705', '655054');
+$getPouleClassementD1_P1 = $service->getPouleClassement('125705', '655053');
+$getPouleClassementD2_P6 = $service->getPouleClassement('125705', '655058');
+?>
 
-        <h2>Club</h2>
-        <table style="text-align: center; width: 100%; margin: auto;">
-            <tbody>
-                <tr class="tabtitre">
-                    <td colspan="2">
-                        <b><?php echo mb_convert_encoding($clubDetails['nom'], 'HTML-ENTITIES', 'UTF-8'); ?></b><br>
-                        n°<?php echo mb_convert_encoding($clubDetails['numero'], 'HTML-ENTITIES', 'UTF-8'); ?>
-                    </td>
-                </tr>
-                <tr class="tabcouleur1">
-                    <td>Salle</td>
-                    <td><?php echo mb_convert_encoding($clubDetails['adressesalle1'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
-                </tr>
-                <tr class="tabcouleur1">
-                    <td>Code postal</td>
-                    <td><?php echo mb_convert_encoding($clubDetails['codepsalle'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
-                </tr>
-                <tr class="tabcouleur2">
-                    <td>Ville</td>
-                    <td><?php echo mb_convert_encoding($clubDetails['villesalle'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
-                </tr>
-                <tr class="tabcouleur1">
-                    <td>Latitude</td>
-                    <td><?php echo mb_convert_encoding($clubDetails['latitude'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
-                </tr>
-                <tr class="tabcouleur2">
-                    <td>Longitude</td>
-                    <td><?php echo mb_convert_encoding($clubDetails['longitude'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
-                </tr>
-                <tr class="tabcouleur1">
-                    <td colspan="2"><a href="http://maps.google.fr/maps?f=q&amp;hl=fr&amp;q=<?php echo mb_convert_encoding($clubDetails['latitude'], 'HTML-ENTITIES', 'UTF-8'); ?>,<?php echo mb_convert_encoding($clubDetails['longitude'], 'HTML-ENTITIES', 'UTF-8'); ?>" title=" Afficher la carte " target="_blank" rel="noopener"><img src="../images/map.png" width="16px" height="16px" alt="" style="vertical-align:middle;"> Afficher la carte</a></td>
-                </tr>
-                <tr class="tabtitre">
-                    <td colspan="2">Contact</td>
-                </tr>
-                <tr class="tabcouleur1">
-                    <td>Prénom</td>
-                    <td><?php echo mb_convert_encoding($clubDetails['prenomcor'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
-                </tr>
-                <tr class="tabcouleur2">
-                    <td>NOM</td>
-                    <td><?php echo mb_convert_encoding($clubDetails['nomcor'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
-                </tr>
-                <tr class="tabcouleur1">
-                    <td>Téléphone</td>
-                    <td><?php echo mb_convert_encoding($clubDetails['telcor'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
-                </tr>
-                <tr class="tabcouleur2">
-                    <td>Mail</td>
-                    <td><a href="mailto:<?php echo mb_convert_encoding($clubDetails['mailcor'], 'HTML-ENTITIES', 'UTF-8'); ?>" title="<?php echo mb_convert_encoding($clubDetails['mailcor'], 'HTML-ENTITIES', 'UTF-8'); ?>"><?php echo mb_convert_encoding($clubDetails['mailcor'], 'HTML-ENTITIES', 'UTF-8'); ?></a></td>
-                </tr>
-                <tr class="tabcouleur1">
-                    <td>Site internet</td>
-                    <td><a href="https://asmeudontennisdetable.fr">https://asmeudontennisdetable.fr</a></td>
-                </tr>
-            </tbody>
-        </table>
+<!-- <div class="table-club">
+    <h2>Club</h2>
+    <table style="text-align: center; width: 100%; margin: auto;">
+        <tbody>
+            <tr class="tabtitre">
+                <td colspan="2">
+                    <b><?php echo mb_convert_encoding($clubDetails['nom'], 'HTML-ENTITIES', 'UTF-8'); ?></b><br>
+                    n°<?php echo mb_convert_encoding($clubDetails['numero'], 'HTML-ENTITIES', 'UTF-8'); ?>
+                </td>
+            </tr>
+            <tr class="tabcouleur1">
+                <td>Salle</td>
+                <td><?php echo mb_convert_encoding($clubDetails['adressesalle1'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
+            </tr>
+            <tr class="tabcouleur1">
+                <td>Code postal</td>
+                <td><?php echo mb_convert_encoding($clubDetails['codepsalle'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
+            </tr>
+            <tr class="tabcouleur2">
+                <td>Ville</td>
+                <td><?php echo mb_convert_encoding($clubDetails['villesalle'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
+            </tr>
+            <tr class="tabcouleur1">
+                <td>Latitude</td>
+                <td><?php echo mb_convert_encoding($clubDetails['latitude'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
+            </tr>
+            <tr class="tabcouleur2">
+                <td>Longitude</td>
+                <td><?php echo mb_convert_encoding($clubDetails['longitude'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
+            </tr>
+            <tr class="tabcouleur1">
+                <td colspan="2"><a href="http://maps.google.fr/maps?f=q&amp;hl=fr&amp;q=<?php echo mb_convert_encoding($clubDetails['latitude'], 'HTML-ENTITIES', 'UTF-8'); ?>,<?php echo mb_convert_encoding($clubDetails['longitude'], 'HTML-ENTITIES', 'UTF-8'); ?>" title=" Afficher la carte " target="_blank" rel="noopener"><img src="../images/map.png" width="16px" height="16px" alt="" style="vertical-align:middle;"> Afficher la carte</a></td>
+            </tr>
+            <tr class="tabtitre">
+                <td colspan="2">Contact</td>
+            </tr>
+            <tr class="tabcouleur1">
+                <td>Prénom</td>
+                <td><?php echo mb_convert_encoding($clubDetails['prenomcor'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
+            </tr>
+            <tr class="tabcouleur2">
+                <td>NOM</td>
+                <td><?php echo mb_convert_encoding($clubDetails['nomcor'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
+            </tr>
+            <tr class="tabcouleur1">
+                <td>Téléphone</td>
+                <td><?php echo mb_convert_encoding($clubDetails['telcor'], 'HTML-ENTITIES', 'UTF-8'); ?></td>
+            </tr>
+            <tr class="tabcouleur2">
+                <td>Mail</td>
+                <td><a href="mailto:<?php echo mb_convert_encoding($clubDetails['mailcor'], 'HTML-ENTITIES', 'UTF-8'); ?>" title="<?php echo mb_convert_encoding($clubDetails['mailcor'], 'HTML-ENTITIES', 'UTF-8'); ?>"><?php echo mb_convert_encoding($clubDetails['mailcor'], 'HTML-ENTITIES', 'UTF-8'); ?></a></td>
+            </tr>
+            <tr class="tabcouleur1">
+                <td>Site internet</td>
+                <td><a href="https://asmeudontennisdetable.fr">https://asmeudontennisdetable.fr</a></td>
+            </tr>
+        </tbody>
+    </table>
+</div> -->
 
-        <h2>Équipes</h2>
-        <table style="text-align: center; width: 100%; margin: auto;">
+<div class="tables table-equipes">
+    <h2>Équipes</h2>
+        <table>
             <tr class="tabtitre">
                 <td>Nom équipe</td>
                 <td>Phase</td>
                 <td>Division</td>
             </tr>
 
-            <?php
-            $compteur = 1; // Initialisation du compteur
+    <?php
+    $compteur = 1; // Initialisation du compteur
 
-            foreach ($equipesByClub as $equipe) :
-                $nomPage = 'meudon' . $compteur;
-            ?>
-                <tr class="tabcouleur1">
-                    <td>
-                        <a href="parties/<?php echo $nomPage; ?>.php">
-                            <?php echo $equipe['libequipe']; ?>
-                            <br>
-                            voir les résultats
-                        </a>
-                    </td>
-                    <td>1</td>
-                    <td><?php echo $equipe['libdivision']; ?></td>
-                </tr>
-            <?php
-                $compteur++;
-            endforeach;
-            ?>
+    foreach ($equipesByClub as $equipe) :
+        $nomPage = 'meudon' . $compteur;
+    ?>
+            <tr class="tabcouleur1">
+                <td>
+                    <p><?php echo $equipe['libequipe']; ?></p>
+                    <a href="parties/<?php echo $nomPage; ?>.php">
+                        voir les résultats
+                    </a>
+                </td>
+                <td>1</td>
+                <td><?php echo $equipe['libdivision']; ?></td>
+            </tr>
+    <?php
+        $compteur++;
+    endforeach;
+    ?>
 
         </table>
+</div>
 
-        <h2>Résultat Equipes</h2>
+<div class="tables table-classement">
+    <h2>Résultat Equipes</h2>
 
-        <?php
-        // Tableau des classements de poules
-        $poulesClassements = array(
-            $getPouleClassementR1,
-            $getPouleClassementR2,
-            $getPouleClassementD1_P1,
-            $getPouleClassementD1_P2,
-            $getPouleClassementD2_P6,
-        );
+    <?php
+    // Tableau des classements de poules
+    $poulesClassements = array(
+        $getPouleClassementR1,
+        $getPouleClassementR2,
+        $getPouleClassementD1_P1,
+        $getPouleClassementD1_P2,
+        $getPouleClassementD2_P6,
+    );
 
-        foreach ($poulesClassements as $classementPoule) :
-        ?>
-            <table style="text-align: center; width: 100%; margin: auto;">
-                <tr class="tabtitre">
-                    <td colspan="2">
-                        <b>Poule <?php echo $classementPoule[0]['poule']; ?></b>
-                    </td>
-                </tr>
-                <tr class="tabcouleur1">
-                    <td>Classement</td>
-                    <td>Équipe</td>
-                </tr>
-                <?php foreach ($classementPoule as $equipeClassement) : ?>
-                    <tr class="tabcouleur1">
-                        <td><?php echo $equipeClassement['clt']; ?></td>
-                        <td>
-                            <?php
-                            // Vérifier si l'ID du club est égal à 12920067
-                            if ($equipeClassement['idclub'] == '12920067') {
-                                echo '<strong>' . mb_convert_encoding($equipeClassement['equipe'], 'HTML-ENTITIES', 'UTF-8') . '</strong>';
-                            } else {
-                                echo mb_convert_encoding($equipeClassement['equipe'], 'HTML-ENTITIES', 'UTF-8');
-                            }
-                            ?>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            </table>
-        <?php endforeach; ?>
+    foreach ($poulesClassements as $classementPoule) :
+    ?>
+    <table>
+        <tr class="tabtitre">
+            <td colspan="2">
+                <b>Poule <?php echo $classementPoule[0]['poule']; ?></b>
+            </td>
+        </tr>
+        <tr class="tabcouleur1">
+            <td>Classement</td>
+            <td>Équipe</td>
+        </tr>
+        <?php foreach ($classementPoule as $equipeClassement) : ?>
+        <tr class="tabcouleur1">
+            <td><?php echo $equipeClassement['clt']; ?></td>
+                <td>
+                    <?php
+                    // Vérifier si l'ID du club est égal à 12920067
+                    if ($equipeClassement['idclub'] == '12920067') {
+                        echo '<strong>' . mb_convert_encoding($equipeClassement['equipe'], 'HTML-ENTITIES', 'UTF-8') . '</strong>';
+                    } else {
+                        echo mb_convert_encoding($equipeClassement['equipe'], 'HTML-ENTITIES', 'UTF-8');
+                    }
+                    ?>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+    <?php endforeach; ?>
+</div>
+</main>
 
-    </main>
     <footer>
         <div>
             <h4>Menu</h4>
